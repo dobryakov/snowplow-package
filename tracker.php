256f5893d4d9447813d677e7c39d1be31b89a99f
Grigoriy Dobryakov <grigoriy@dobryakov.com>
2014-11-02 14:50:51 +0400

    add gitignore

--- tracker.php
+++ tracker.php
@@ -4,8 +4,10 @@ if (empty($_COOKIE['sp'])) {
   setcookie('sp', uniqid(), time()+60*60*24*30, '/');
 }
 
-//header('P3P: CP="CURa ADMa DEVa PSAo PSDo OUR BUS UNI PUR INT DEM STA PRE COM NAV OTC NOI DSP COR"');
-//header('P3P: CP="CAO PSA OUR"');
+$r = $_SERVER['REQUEST_URI'] . '&nuid=' . $_COOKIE['sp'];
+
+file_put_contents(dirname(__FILE__) . '/sp.log', $r . "\n", FILE_APPEND);
+
 header('P3P: policyref="/w3c/p3p.xml", CP="NOI DSP COR NID PSA OUR IND COM NAV STA"');
 header("Content-type: image/gif");
 readfile(dirname(__FILE__) . "/i");
