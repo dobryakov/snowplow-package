e1ff086e81604a97f247ed3092d871739f8bba22
user <user@srv04.adaliskanetwork.com>
2015-06-22 11:36:42 -0400

    minor update

--- mysql-persist.php
+++ mysql-persist.php
@@ -1,6 +1,6 @@
 <?php
 
-$query = 'INSERT INTO raw (request) VALUES ("' . mysql_escape_string($r) . '")';
+$query = 'INSERT INTO raw (request, created_at) VALUES ("' . mysql_escape_string($r) . '", ' . time() . ')';
 
 $result = mysql_query($query) or die('Запрос не удался: ' . mysql_error());
 
